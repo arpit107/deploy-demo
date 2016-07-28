88495036251ef541991c65bd431398ea7f76a0e6
arpit107 <arpit.29082011@gmail.com>
2016-07-28 16:25:12 +0530

    commit

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + "/src"));
-app.listen(process.env.PORT || 3000);
-console.log("Server up on port 3000");
+app.listen(process.env.PORT || 7000);
+console.log("Server up on port 7000");
